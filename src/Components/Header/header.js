9c2a70fddec5ccc09af3aca2a2fb3d15c9f64a82
mahesharya123 <mahesharya381@gmail.com>
2025-02-13 02:51:17 +0530

    develop the cart functionality

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,6 +1,6 @@
 import './header.css';
-
-export const Header =()=>{
+import { Cart } from '../Cart';
+export const Header =({count,items,onHandleEvent})=>{
    
 return(
     <>
@@ -42,17 +42,7 @@ return(
             </svg>
         </div>
         <div className="cart-container">
-            <button>
-                <span>Cart</span>
-                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-shopping-cart-plus" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <circle cx="6" cy="19" r="2" />
-                    <circle cx="17" cy="19" r="2" />
-                    <path d="M17 17h-11v-14h-2" />
-                    <path d="M6 5l6.005 .429m7.138 6.573l-.143 .998h-13" />
-                    <path d="M15 6h6m-3 -3v6" />
-                </svg>
-            </button>
+           <Cart count={count} items={items} onHandleEvent={onHandleEvent} />
         </div>
     </header>
     </>
--- src/Components/ListItems/ListItems.js
+++ src/Components/ListItems/ListItems.js
@@ -1,34 +1,40 @@
 
 import { useState } from 'react';
 import cartitem from '../../assets/cart.png';
+import { Modal } from '../UI/Modal';
 
 import     './ListItems.css';
 
   
-export const Lisitems =({data})=>{
+export const Listitems =({data ,onAdd,onRemove})=>{
+          
+  
+//const [counter, setCounter] = useState(0);
+const [showModal,setshowModal] =useState(false);
 
-    const [isAdded, setIsAdded] = useState(false);
-const [counter, setCounter] = useState(1);
-const handleClick = () => {
-    setIsAdded(true); // Show the quantity buttons
-};
 
-const descreaseCounterByOne = () => {
-    if (counter > 1) {
+const descreaseCounterByOne = (event) => {
+    event.stopPropagation();
+    onRemove(data.id);
+    /*if(counter === 1) onRemove(data.id);
+    else if (counter > 1) {
         setCounter(counter - 1);
-    } else {
-        setIsAdded(false); // Hide counter and show "Add to Cart" when quantity is 0
-    }
+    } */
 };
 
-const increaseCounterByOne = () => {
-    setCounter(counter + 1);
+const increaseCounterByOne = event => {
+    event.stopPropagation();
+    onAdd(data.id);
+   // setCounter(counter + 1);
 };
-
+  const handleModal=()=>{
+    setshowModal(previousState=>!previousState)
+    console.log(data.description);
+  }
     return (
         <>
         
-        <div className='card'>
+        <div  onClick={handleModal} className='card'>
              <div className='img-card'> 
                 <img  className='img' src ={`/assets/${data.thumbnail}`} alt='watch'/>
                 
@@ -42,15 +48,15 @@ const increaseCounterByOne = () => {
                     <span>{data.title} </span>
                 </div>
                 <div>
-                {!isAdded ? (
-                <button className="cart-add" onClick={handleClick}>
+                {data.quantity<1? (
+                <button className="cart-add" onClick={increaseCounterByOne}>
                     <span>Add to Cart</span>
                     <img src={cartitem} alt="Cart Icon"/>
                 </button>
             ) : (
                 <div className="cart-addon">
                     <button onClick={descreaseCounterByOne}><span>-</span></button>
-                    <span className="counter">{counter}</span>
+                    <span className="counter">{data.quantity}</span>
                     <button onClick={increaseCounterByOne}><span>+</span></button>
                 </div>
             )}
@@ -58,6 +64,49 @@ const increaseCounterByOne = () => {
                 </div>
               </div>
         </div>
+        {showModal && <Modal onClose={handleModal}> 
+
+        <div className="modal-pop">
+                        <div className="img-wrap">
+                            <img className='img-fluid' src={`/assets/${data.thumbnail}`} alt={data.title}/>
+                        </div>
+                        <div className='disc'> <p>{data.description}</p></div>
+                        </div>
+                        <div className="meta">
+                            <div className='p-t'>
+                            <h3>{data.title}</h3>
+                            <div className="price">
+                               <b><span>{data.discountedprice}</span></b> 
+
+                                <sup>
+                                   <i><strike>{data.price}</strike></i> 
+                                </sup>
+                               
+                            </div>
+                            </div>
+                            <div className='adcart'>
+                            {data.quantity <1? 
+                <button className="cart-add" onClick={increaseCounterByOne}>
+                    <span>Add to Cart</span>
+                    <img src={cartitem} alt="Cart Icon"/>
+                </button>
+             : 
+                <div className="cart-addon">
+                    <button onClick={descreaseCounterByOne}><span>-</span></button>
+                    <span className="counter">{data.quantity}</span>
+                    <button onClick={increaseCounterByOne}><span>+</span></button>
+                    
+                </div>
+                            
+           
+                            
+                         
+               
+            } </div> 
+            </div>
+        
+                    
+            </Modal>}
         </>
     )
 }
